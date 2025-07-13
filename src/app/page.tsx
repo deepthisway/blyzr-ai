fa9aef3554868bd21a9baaeaa54a04cd40a243e1
deepthisway <contactdeep01@gmail.com>
2025-07-14 00:11:54 +0530

    add tRPC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+
+import { caller } from '@/trpc/server'
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 
-const Page = () => {
+const Page = async () => {
+  const data = await caller.hello({text: "QTTTTT"})
   return (
-    <div>Hello World</div>
+    <div>{JSON.stringify(data)}</div>
   )
 }
 
