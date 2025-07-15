c1898ee516f049b81cbdf545d060655e95362707
deepthisway <contactdeep01@gmail.com>
2025-07-15 14:56:01 +0530

    add Agent tools

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -5,6 +5,7 @@ import { Sandbox } from "@e2b/code-interpreter";
 import { getSandbox } from "./util";
 import { stderr, stdout } from "process";
 import z from "zod";
+import { PROMPT } from "@/prompt";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -16,9 +17,9 @@ export const helloWorld = inngest.createFunction(
     });
 
     const writer = createAgent({
-      name: "writer",
-      system:
-        "You are an expert writer. You write readable, concise, simple content.",
+      name: "coding-agent",
+      description: "An expert coding agent that writes code.",
+      system: PROMPT,
       model: gemini({ model: "gemini-2.5-flash" }),
       tools:[
         createTool({
@@ -32,7 +33,7 @@ export const helloWorld = inngest.createFunction(
             // step and command are coming from two diffent objs, hence seperated destructuring
             return await step?.run("terminal", async()=>{
               const buffers = {stdout: "", stderr: ""};
-
+ 
               try {
                 const sandbox = await getSandbox(sandboxId);
                 const result = await sandbox.commands.run(command, {
@@ -51,9 +52,70 @@ export const helloWorld = inngest.createFunction(
                   return `Command Failed: ${e} \n stderr: ${buffers.stderr} \n stdout: ${buffers.stdout}`
               }
             })
+          },
+        }),
+        createTool({
+          name: "createOrUpdateFiles",
+          description: "Create or update files in the sandbox",
+          parameters: z.object({
+            files: z.array(
+              z.object({ //  each file has a path and content
+                path: z.string(),
+                content: z.string(),
+              })
+            ),
+          }) as any,
+          handler: async ({ files }, { step, network }) => {
+            const result = await step?.run("createOrUpdateFiles", async () => {
+              try {
+                const updatedFiles = network.state.data.files || {}; // used to track file state in memory (like a session store).
+                /*
+                {
+                  "/hello.txt": "Hello World",
+                  "/main.js": "console.log('Hi');" is an example of the updatedFiles object
+                }
+                */
+                const sandbox = await getSandbox(sandboxId);
+                for (const file of files) {
+                  await sandbox.files.write(file.path, file.content);
+                  updatedFiles[file.path] = file.content;
+                }
+                return updatedFiles;
+              } catch (error) {
+                return `Error updating files: ${error}`;
+              }
+            });
+
+            if (result && typeof result === "object") {
+              network.state.data.files = result;
+            }
+
+            return `Files updated.`;
+          },
+        }),
+        createTool({
+          name: "readFiles",
+          description: "Read files from the sandbox",
+          parameters: z.object({
+            files: z.array(z.string())
+          }),
+          handler: async ({files}, {step})  => {
+            return await step?.run("readFiles", async ()=>  {
+              try {
+                const sandBox =await getSandbox(sandboxId);
+                const contents = [];
+                for(const file of files)  {
+                  const content = await sandBox.files.read(file);
+                  contents.push({path: file, content});
+                }
+                return JSON.stringify(contents);
+              } catch (error) {
+                return `Error reading files: ${error}`;
+              }
+            })
           }
         })
-      ]
+      ],
     });
 
     const summary = await writer.run(
